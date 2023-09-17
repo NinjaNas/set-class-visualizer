4ddfd7555c7f7931908f7d0c374e7adb253bad7f
Khang Tran <ktra@email.unc.edu>
2023-09-17 17:56:44 -0400

    remove fontawesome

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,4 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// import the fontawesome core and import config so we can set a default style
-import { library, config } from '@fortawesome/fontawesome-svg-core'
-
-// import fontawesome icon component
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-// import icon style(s)
-import { faSun, faMoon } from '@fortawesome/free-regular-svg-icons'
-
-// add the icon style(s) you have installed to the library
-library.add(faSun, faMoon)
-
-config.styleDefault = 'regular'
-
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).mount('#app')
