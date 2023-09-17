c37baed7d7b4a3166730cff1fd2da75beb419d6d
Khang Tran <ktra@email.unc.edu>
2023-09-17 17:45:57 -0400

    added options tab component

---
+++ src/components/OptionsTab.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import LightDarkModeButton from './LightDarkModeButton.vue'
+</script>
+
+<template>
+  <div>
+    <LightDarkModeButton></LightDarkModeButton>
+  </div>
+</template>
+
+<style>
+.toggle-container {
+  display: flex;
+  gap: 10px;
+  padding: 1%;
+}
+</style>
